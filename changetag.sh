d881851c80a7d3590a9da814c38b40f58b7ec511
Elyes00 <elyes.benamor@etudiant-enit.utm.tn>
2019-11-28 00:05:22 +0100

    changetag updated

--- changetag.sh
+++ changetag.sh
@@ -2,3 +2,4 @@
 sed "s/tagVersion/$1/g" memcached.yml > memcached.yml
 sed "s/tagVersion/$1/g" nginx.yml > nginx.yml
 sed "s/tagVersion/$1/g" sentry-web.yml > sentry-web.yml
+sed "s/tagVersion/$1/g" postgres.yml > postgres.yml
