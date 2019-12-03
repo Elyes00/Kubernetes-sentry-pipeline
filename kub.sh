a18e1a88d86c5b3fdc3f3d5de681f99cd1bf8e91
elyes ben amor <45462730+Elyes00@users.noreply.github.com>
2019-12-03 14:25:52 +0100

    Update kub.sh

--- kub.sh
+++ kub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl run sentry-worker --image=sentry:9.0-onbuild --rm -i --tty \
+kubectl run sentry-worker --image=elyes000/sentry:${DOCKER_TAG} --rm -i --tty \
 --env="SENTRY_MEMCACHED_HOST=service-sentry-memcached" \
 --env="SENTRY_REDIS_HOST=service-sentry-redis" \
 --env="SENTRY_POSTGRES_HOST=service-sentry-pg" \
